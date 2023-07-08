524e28f7afd7231b17485e0b6ce8b180ad25d35e
jonarroh <jonarrodi99@gmail.com>
2023-07-07 20:35:04 -0600

    ✨ feat (app) Conver image in webp

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 ## Description
 
 Demo del uso de View Transitions WEB API
-basado en el video de [GEl FUTURO de la PROGRAMACIÓN WEB: View Transitions API](https://youtu.be/ODv-VFRFGcY)
+basado en el video de [El FUTURO de la PROGRAMACIÓN WEB: View Transitions API](https://youtu.be/ODv-VFRFGcY)
--- src/db/images.ts
+++ src/db/images.ts
@@ -9,42 +9,42 @@ interface dbImages {
 export const dbImages: dbImages[] = [
 	{
 		id: 'a_marte',
-		image: '/a_marte.jpg',
+		image: '/a_marte.webp',
 		title: 'A Marte',
 		IA: 'dreamboot',
 		descripcion: 'Imagen de marte porque solo queria ir a marte'
 	},
 	{
 		id: 'cocogato',
-		image: 'cocogato.png',
+		image: 'cocogato.webp',
 		title: 'Cocogato',
 		IA: 'Edge image creator',
 		descripcion: 'Un cocogato solo eso un cocogato'
 	},
 	{
 		id: 'deidad',
-		image: 'deidad.png',
+		image: 'deidad.webp',
 		title: 'Deidad',
 		IA: 'Edge image creator',
 		descripcion: 'Representacion de una deidad'
 	},
 	{
 		id: 'depresion',
-		image: 'depresion.jpg',
+		image: 'depresion.webp',
 		title: 'Depresion',
 		IA: 'dreamboot',
 		descripcion: 'Representacion de la depresion'
 	},
 	{
 		id: 'life',
-		image: 'life.jpg',
+		image: 'life.webp',
 		title: 'Life',
 		IA: 'dreamboot',
 		descripcion: 'Representacion de como un gato ve la vida'
 	},
 	{
 		id: 'space_dogo',
-		image: 'space_dogo.jpg',
+		image: 'space_dogo.webp',
 		title: 'Space Dogo',
 		IA: 'dreamboot',
 		descripcion: 'Un perro en el espacio'
